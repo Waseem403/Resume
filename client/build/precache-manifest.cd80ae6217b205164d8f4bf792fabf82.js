3d80f0844dadc9b9c2067fc5e8126b4e9105787c
waseem shaikh <b.waseem.403@gmail.com>
2020-02-24 20:32:47 +0530

    build folder added

---
+++ client/build/precache-manifest.cd80ae6217b205164d8f4bf792fabf82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1a839f38d2bb9d5d64e4920715d50e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ceca39ef5077a71a7821",
+    "url": "/static/css/2.66c0e78c.chunk.css"
+  },
+  {
+    "revision": "58fa3344e215310a2e12",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ceca39ef5077a71a7821",
+    "url": "/static/js/2.538cfeb9.chunk.js"
+  },
+  {
+    "revision": "88e55fa2a30baba8872ebcda29eb845e",
+    "url": "/static/js/2.538cfeb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58fa3344e215310a2e12",
+    "url": "/static/js/main.6156ab75.chunk.js"
+  },
+  {
+    "revision": "784f934da7cae9e13356",
+    "url": "/static/js/runtime-main.1a63ad0f.js"
+  }
+]);
