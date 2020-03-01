4c677329edc46629162444403ce02f560e213c5f
Aleem Shaikh <b.waseem.403@gmail.com>
2020-03-01 22:51:08 +0530

    updated build folder

---
+++ client/build/precache-manifest.e369e07d560d8af86a7abf9e7829b1cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "004fa0d1d2ce5bbee62607c1fb7d9949",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1eab54229125f24e2e47",
+    "url": "/static/css/2.143a0508.chunk.css"
+  },
+  {
+    "revision": "6b66df6e2ae3c209b467",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "1eab54229125f24e2e47",
+    "url": "/static/js/2.df48796f.chunk.js"
+  },
+  {
+    "revision": "88e55fa2a30baba8872ebcda29eb845e",
+    "url": "/static/js/2.df48796f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b66df6e2ae3c209b467",
+    "url": "/static/js/main.b85343ca.chunk.js"
+  },
+  {
+    "revision": "784f934da7cae9e13356",
+    "url": "/static/js/runtime-main.1a63ad0f.js"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cd80ae6217b205164d8f4bf792fabf82.js"
+  "/precache-manifest.e369e07d560d8af86a7abf9e7829b1cc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
