474951865f9d64194ddb37f4031571fd15da6dc0
waseem shaikh <b.waseem.403@gmail.com>
2020-03-02 17:05:37 +0530

    Updated client folder

---
+++ client/build/precache-manifest.9fee65def305d8465290e0483052ae6b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c21a81ad5b82d1ca7e43d385e626826d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1dc3f5e85e699b5d3ae6",
+    "url": "/static/css/2.66c0e78c.chunk.css"
+  },
+  {
+    "revision": "b9fc564e76ae888dc114",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1dc3f5e85e699b5d3ae6",
+    "url": "/static/js/2.a06d184d.chunk.js"
+  },
+  {
+    "revision": "88e55fa2a30baba8872ebcda29eb845e",
+    "url": "/static/js/2.a06d184d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9fc564e76ae888dc114",
+    "url": "/static/js/main.de91de88.chunk.js"
+  },
+  {
+    "revision": "784f934da7cae9e13356",
+    "url": "/static/js/runtime-main.1a63ad0f.js"
+  },
+  {
+    "revision": "6056847f66d95fd33286aef6a3efaa43",
+    "url": "/static/media/waseem.6056847f.jpg"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c190adbe5c36e0d0c5dc51e2bd2a4117.js"
+  "/precache-manifest.9fee65def305d8465290e0483052ae6b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
