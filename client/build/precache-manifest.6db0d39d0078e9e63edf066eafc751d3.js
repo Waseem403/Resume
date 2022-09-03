d66bab73bf4975bf60bfe771e1220c803b586837
waseem shaikh <33866741+Waseem403@users.noreply.github.com>
2022-09-03 21:01:33 +0530

    Add files via upload

---
+++ client/build/precache-manifest.6db0d39d0078e9e63edf066eafc751d3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6364cbbd9bd49a8616c7ac63c8e1f3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22f53452b48235958e28",
+    "url": "/static/css/2.b312678f.chunk.css"
+  },
+  {
+    "revision": "165425d9baa4f1ec5b77",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "22f53452b48235958e28",
+    "url": "/static/js/2.09c3c092.chunk.js"
+  },
+  {
+    "revision": "e933f8dbfdf496cb72e495663a499a00",
+    "url": "/static/js/2.09c3c092.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "165425d9baa4f1ec5b77",
+    "url": "/static/js/main.77a231a7.chunk.js"
+  },
+  {
+    "revision": "784f934da7cae9e13356",
+    "url": "/static/js/runtime-main.1a63ad0f.js"
+  },
+  {
+    "revision": "1ea22d6a11c8330e48f8f90ae4be38dc",
+    "url": "/static/media/waseem.1ea22d6a.jpg"
+  }
+]);
